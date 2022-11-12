496dc23ded1913784a49fc380947dc9da4b2ab94
Saubhagya <sikan2601@gmail.com>
2022-11-12 13:27:08 +0530

    basic version ready3

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 const User = require("../model/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { response } = require("express");
 
 module.exports.register = async (req, res, next) => {
